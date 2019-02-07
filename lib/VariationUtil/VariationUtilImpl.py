e44ca9ed54207ecb85a7f99920fbeccd408a8980
Ryan Routsong <rroutson@utk.edu>
2019-02-07 17:40:09 -0500

    lets output submodules

--- lib/VariationUtil/VariationUtilImpl.py
+++ lib/VariationUtil/VariationUtilImpl.py
@@ -5,6 +5,20 @@ import os
 import sys
 from pprint import pprint
 
+import imp
+MODULE_EXTENSIONS = ('.py', '.pyc', '.pyo')
+
+def package_contents(package_name):
+    file, pathname, description = imp.find_module(package_name)
+    if file:
+        raise ImportError('Not a package: %r', package_name)
+    # Use a set because some may be both source and compiled.
+    return set([os.path.splitext(module)[0]
+        for module in os.listdir(pathname)
+        if module.endswith(MODULE_EXTENSIONS)])
+
+exit(package_contents('VariationUtil'))
+
 from installed_clients.KBaseReportClient import KBaseReport
 
 from VariationUtil.VariationToVCF import VariationToVCF

2f9e01bfcef62a8655aca5a37b9d4ba26ca40771
Ryan Routsong <rroutson@utk.edu>
2019-02-08 16:30:39 -0500

    listing /staging

--- lib/VariationUtil/VCFtoVariation.py
+++ lib/VariationUtil/VCFtoVariation.py
@@ -5,7 +5,7 @@ import subprocess
 import logging
 import time
 
-exit(os.listdir('/'))
+exit(os.listdir('/staging'))
 
 from installed_clients.DataFileUtilClient import DataFileUtil
 from installed_clients.WorkspaceClient import Workspace

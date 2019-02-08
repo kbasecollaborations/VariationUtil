b95bf90a019a212544eb15877ea17a7676380eba
Ryan Routsong <rroutson@utk.edu>
2019-02-08 15:37:14 -0500

    exit to list /kb/module/data

--- lib/VariationUtil/VCFtoVariation.py
+++ lib/VariationUtil/VCFtoVariation.py
@@ -5,7 +5,7 @@ import subprocess
 import logging
 import time
 
-exit(os.listdir('/data'))
+exit(os.listdir('/kb/module/data'))
 
 from installed_clients.DataFileUtilClient import DataFileUtil
 from installed_clients.WorkspaceClient import Workspace
